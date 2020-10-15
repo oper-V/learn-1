7a6e4fb7f29c116e475d5a43f5ec7ff202323a61
oper-V <igor.prohorenko@gmail.com>
2020-10-15 23:54:35 +0300

    Исходное состояние для задания по interface ListIterator<E>

--- src/ArrayCollection.java
+++ src/ArrayCollection.java
@@ -1,13 +1,15 @@
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.NoSuchElementException;
 import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
 
-public class ArrayCollection<T> implements Collection<T> {
+public class ArrayList<T> implements List<T> {
 
-    private T[] m = (T[])new Object[1];
+    private T[] array = (T[]) new Object[1];
 
-    private int size;
+    private int size = 0;
 
     @Override
     public int size() {
@@ -22,7 +24,9 @@ public class ArrayCollection<T> implements Collection<T> {
     @Override
     public boolean contains(final Object o) {
         for (int i = 0; i < size; i++) {
-            if (m[i].equals(o)) return true;
+            if (array[i].equals(o)) {
+                return true;
+            }
         }
         return false;
     }
@@ -33,49 +37,77 @@ public class ArrayCollection<T> implements Collection<T> {
     }
 
     @Override
-    public Object[] toArray() {
-        final T[] newM = (T[])new Object[this.size()];
-        System.arraycopy(m, 0, newM, 0, this.size());
+    public T[] toArray() {
+        final T[] newM = (T[]) new Object[this.size()];
+        System.arraycopy(array, 0, newM, 0, this.size());
         return newM;
     }
 
     @Override
-    public <T> T[] toArray(final T[] a) {
-        // BEGIN (write your solution here)
-        if (a.length == size()) {
-            System.arraycopy(m, 0, a, 0, this.size());
+    public <T1> T1[] toArray(final T1[] a) {
+        if (a.length < size) {
+            return (T1[]) Arrays.copyOf(array, size, a.getClass());
         }
-        else if (a.length > size()) {
-            System.arraycopy(m, 0, a, 0, this.size());
-            add(null);
+        System.arraycopy(array, 0, a, 0, size);
+        if (a.length > size) {
+            a[size] = null;
         }
-        else if (a.length < size()) {
-            T[] newA =  Arrays.copyOf(a,this.size());
-//            T[] newA = new T[this.size()];
-            System.arraycopy(m, 0, newA, 0, this.size());
-            return  newA;
-        }
-
-        return  a;
-
-        // END
+        return a;
     }
 
     @Override
     public boolean add(final T t) {
-        if (m.length == size) {
-            final T[] oldM = m;
-            m = (T[]) new Object[this.size() * 2];
-            System.arraycopy(oldM, 0, m, 0, oldM.length);
+        if (array.length == size) {
+            final T[] oldM = array;
+            array = (T[]) new Object[this.size() * 2];
+            System.arraycopy(oldM, 0, array, 0, oldM.length);
         }
-        m[size++] = t;
+        array[size++] = t;
+        return true;
+    }
+
+    @Override
+    public void add(final int index, final T element) {
+        if (index > size || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (size  == 0 || index == size) {
+            add(element);
+        } else if (array.length == size) {
+            final T[] tempM = array;
+            array = (T[]) new Object[this.size() * 2];
+
+            System.arraycopy(tempM, 0, array, 0,  index);
+            System.arraycopy(tempM, index, array, index + 1, size() - index);
+
+            set(index, element);
+            size++;
+        } else {
+            final T[] tempM = array;
+            System.arraycopy(tempM, 0, array, 0, index + 1);
+            System.arraycopy(tempM, index, array, index + 1, size() - index);
+            set(index, element);
+            size++;
+        }
+    }
+
+    @Override
+    public boolean addAll(final int index, final Collection elements) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(final Collection<? extends T> c) {
+        for (final T item : c) {
+            add(item);
+        }
         return true;
     }
 
     @Override
     public boolean remove(final Object o) {
         for (int i = 0; i < size(); i++) {
-            if (m[i].equals(o)) {
+            if (array[i].equals(o)) {
                 this.remove(i);
                 return true;
             }
@@ -84,17 +116,21 @@ public class ArrayCollection<T> implements Collection<T> {
     }
 
     @Override
-    public boolean containsAll(final Collection<?> c) {
-        for (final Object item : c) {
-            if (!this.contains(item)) return false;
+    public T remove(final int index) {
+        final T element = array[index];
+        if (index != this.size() - 1) {
+            System.arraycopy(array, index + 1, array, index, this.size() - index - 1);
         }
-        return true;
+        size--;
+        return element;
     }
 
     @Override
-    public boolean addAll(final Collection<? extends T> c) {
-        for (final T item : c) {
-            add(item);
+    public boolean containsAll(final Collection<?> c) {
+        for (final Object item : c) {
+            if (!this.contains(item)) {
+                return false;
+            }
         }
         return true;
     }
@@ -110,48 +146,169 @@ public class ArrayCollection<T> implements Collection<T> {
     @Override
     public boolean retainAll(final Collection<?> c) {
         for (final Object item : this) {
-            if (!c.contains(item)) this.remove(item);
+            if (!c.contains(item)) {
+                this.remove(item);
+            }
         }
         return true;
     }
 
     @Override
     public void clear() {
-        m = (T[])new Object[1];
+        array = (T[]) new Object[1];
         size = 0;
     }
 
-    private void remove(final int index) {
-        if (index != this.size() - 1)
-            System.arraycopy(m, index + 1, m, index, this.size() - index - 1);
-        size--;
+    @Override
+    public List<T> subList(final int start, final int end) {
+        return null;
     }
 
-    private class ElementsIterator implements Iterator<T> {
+    @Override
+    public ListIterator<T> listIterator() {
+        return new ElementsIterator(0);
+    }
 
+    @Override
+    public ListIterator<T> listIterator(final int index) {
+        return new ElementsIterator(index);
+    }
+
+    @Override
+    public int lastIndexOf(final Object target) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int indexOf(final Object target) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public T set(final int index, final T element) {
+        array[index] = element;
+        return element;
+    }
+
+    @Override
+    public T get(final int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        return array[index];
+    }
+
+    private class ElementsIterator implements ListIterator<T> {
+
+        private static final int LAST_IS_NOT_SET = -1;
         private int index;
+        private int lastIndex = LAST_IS_NOT_SET;
 
-        private int last = -1;
+        public ElementsIterator() {
+            this(0);
+        }
+
+        public ElementsIterator(final int index) {
+            // BEGIN (write your solution here)  ElementsIterator
+
+            // END
+        }
 
         @Override
         public boolean hasNext() {
-            return ArrayCollection.this.size() > index;
+            return ArrayList.this.size() > index;
         }
 
         @Override
         public T next() {
-            if (!hasNext())
+            if (!hasNext()) {
                 throw new NoSuchElementException();
-            last = index;
-            return ArrayCollection.this.m[index++];
+            }
+            lastIndex = index++; // or lastIndex = nextIndex(); index++;
+            return ArrayList.this.array[lastIndex];
+        }
+
+        @Override
+        public int nextIndex() {
+            // BEGIN (write your solution here)   nextIndex
+
+            // END
+        }
+
+        @Override
+        public boolean hasPrevious() {
+            // BEGIN (write your solution here)   hasPrevious
+
+            // END
+        }
+
+        @Override
+        public T previous() {
+            // BEGIN (write your solution here)   previous
+
+            // END
+        }
+
+        @Override
+        public int previousIndex() {
+            // BEGIN (write your solution here)  previousIndex
+
+            // END
+        }
+
+        @Override
+        // . a ^ b . c . d . e . f .
+        public void add(final T element) {
+            // BEGIN (write your solution here)  add
+
+            // END
+        }
+
+        @Override
+        public void set(final T element) {
+            // BEGIN (write your solution here)  set
+
+            // END
         }
 
         @Override
         public void remove() {
-            if (last == -1) throw new IllegalStateException();
-            ArrayCollection.this.remove(last);
+            secretRemove();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /////
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void secretRemove() {
+            if (lastIndex == LAST_IS_NOT_SET) {
+                throw new IllegalStateException();
+            }
+            ArrayList.this.remove(lastIndex);
             index--;
-            last = -1;
+            lastIndex = LAST_IS_NOT_SET;
         }
     }
 }
