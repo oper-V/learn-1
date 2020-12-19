f51e650e0568b876879fb078f48db838c6eecb09
oper-V <igor.prohorenko@gmail.com>
2020-12-19 16:31:04 +0200

    Модуль 3. Урок 2. Часть 2 Обновлены файлв классов под задание урока.

--- src/ArrayBasedMap.java
+++ src/ArrayBasedMap.java
@@ -2,144 +2,126 @@ import java.util.*;
 
 public class ArrayBasedMap<K, V> implements Map<K, V> {
 
-    private List<Pair> KeyAndValues = new ArrayList<>();
+    private List<Pair<K, V>> keyAndValues = new ArrayList<>();
 
     @Override
     public int size() {
-        // BEGIN (write your solution here)
-//        int size = 0;
-//        for (Pair p : KeyAndValues) size++;
-//        return size;
-        return KeyAndValues.size();
-
-        // END
+        return keyAndValues.size();
     }
 
     @Override
     public boolean isEmpty() {
-        // BEGIN (write your solution here)
-        return KeyAndValues.size() == 0;
-
-        // END
+        return keyAndValues.isEmpty();
     }
 
     @Override
     public boolean containsKey(Object key) {
-        // BEGIN (write your solution here)
-        K forTestKey = (K) key;
-        for (Pair p : KeyAndValues) {
-            if(forTestKey==null ? p.key==null : p.key.equals(forTestKey)) return true;
+        for (Pair<K, V> p : keyAndValues) {
+            if (p.getKey().equals(key)) {
+                return true;
+            }
         }
-
         return false;
-        // END
     }
 
     @Override
     public boolean containsValue(Object value) {
-        // BEGIN (write your solution here)
-        V forTestValue = (V) value;
-        for (Pair p : KeyAndValues) {
-            if(forTestValue==null ? p.value==null : p.value.equals(forTestValue)) return true;
+        for (Pair<K, V> p : keyAndValues) {
+            if (p.getValue().equals(value)) {
+                return true;
+            }
         }
-
         return false;
-
-        // END
     }
 
     @Override
     public V get(Object key) {
-        // BEGIN (write your solution here)
-        K forTestKey = (K) key;
-        for (Pair p : KeyAndValues) {
-            if(forTestKey==null ? p.key==null : p.key.equals(forTestKey)) {
+        if (key == null) {
+            return null;
+        }
+
+        for (Pair<K, V> p : keyAndValues) {
+            if (p.getKey().equals(key)) {
                 return p.getValue();
             }
         }
         return null;
+    }
+
+    @Override
+    public V getOrDefault(Object key, V defaultValue) {
+        // BEGIN (write your solution here)
 
         // END
     }
 
     @Override
     public V put(K key, V value) {
-        // BEGIN (write your solution here)
-        K forTestKey = (K) key;
-        if(this.containsKey(forTestKey)) {
-            for (Pair p : KeyAndValues) {
-                if(forTestKey==null ? p.key==null : p.key.equals(forTestKey)) return p.setValue(value);
-            }
-
-        } else {
-            Pair newPair = new Pair(key, value);
-            KeyAndValues.add(newPair);
+        V result = value;
+        if (!containsKey(key)) {
+            keyAndValues.add(new Pair<>(key, value));
+            return null;
         }
-        return  null;
-
-        // END
+        for (Pair<K, V> p : keyAndValues) {
+            if (p.getKey().equals(key)) {
+                result = p.getValue();
+                p.setValue(value);
+                break;
+            }
+        }
+        return result;
     }
 
     @Override
     public V remove(Object key) {
-        // BEGIN (write your solution here)
-        K forTestKey = (K) key;
-        if(!this.containsKey(forTestKey)) return null;
-        for (Pair p : KeyAndValues) {
-            if(forTestKey==null ? p.key==null : p.key.equals(forTestKey)) {
-                final V oldValue = p.value;
-                KeyAndValues.remove(p);
-                return oldValue;
+        V value;
+        for (int i = 0; i < keyAndValues.size(); i++) {
+            final Pair<K, V> p = keyAndValues.get(i);
+            if (p.getKey().equals(key)) {
+                value = p.getValue();
+                keyAndValues.remove(i);
+                return value;
             }
         }
         return null;
-        // END
     }
 
     @Override
     public void putAll(Map<? extends K, ? extends V> m) {
-        for (Entry<K, V> e : (Set<Entry<K, V>>)(Set)m.entrySet())
+        for (Map.Entry<K, V> e : (Set<Map.Entry<K, V>>)(Set)m.entrySet()) {
             put(e.getKey(), e.getValue());
+        }
     }
 
     @Override
     public void clear() {
-        // BEGIN (write your solution here)
-        KeyAndValues.clear();
-
-        // END
+        keyAndValues.clear();
     }
 
     @Override
     public Set<K> keySet() {
-        final Set<K> keys = new HashSet<K>();
-        for (Pair p : KeyAndValues) keys.add(p.getKey());
+        final Set<K> keys = new HashSet<>();
+        for (Pair<K, V> p : keyAndValues) {
+            keys.add(p.getKey());
+        }
         return keys;
     }
 
     @Override
     public Collection<V> values() {
-        // BEGIN (write your solution here)
-//        if(KeyAndValues.isEmpty()) {
-//            return null;
-//        } else {
-//            return (Collection<V>) KeyAndValues.subList(0,KeyAndValues.size()-1);
-            Collection<V> collectionForReturn = new ArrayList<>();
-            for (Pair p : KeyAndValues) {
-                collectionForReturn.add(p.value);
-            }
-            return collectionForReturn;
-//        }
-
-        // END
+        final List<V> val = new ArrayList<>();
+        for (Pair<K, V> p : keyAndValues) {
+            val.add(p.getValue());
+        }
+        return val;
     }
 
     @Override
     public Set<Entry<K, V>> entrySet() {
-        return (Set<Entry<K, V>>)(Set)new HashSet<>(KeyAndValues);
+        return new HashSet<>(keyAndValues);
     }
 
-    private class Pair implements Entry<K, V> {
+    private static class Pair<K, V> implements Map.Entry<K, V> {
 
         private final K key;
 
@@ -167,12 +149,16 @@ public class ArrayBasedMap<K, V> implements Map<K, V> {
 
         @Override
         public boolean equals(Object o) {
-            if (this == o) return true;
+            if (this == o) {
+                return true;
+            }
 
-            Entry<K, V> pair = (Entry<K, V>) o;
+            Map.Entry<K, V> pair = (Map.Entry<K, V>) o;
 
 
-            if (key != null ? !key.equals(pair.getKey()) : pair.getKey() != null) return false;
+            if (key != null ? !key.equals(pair.getKey()) : pair.getKey() != null) {
+                return false;
+            }
             return !(value != null ? !value.equals(pair.getValue()) : pair.getValue() != null);
 
         }
